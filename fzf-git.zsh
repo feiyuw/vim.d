c23d2b2153cd71aef303e4b839ff0c966e96625e
Zhang Yu <zhangyu@dingxiang-inc.com>
2019-06-16 17:12:54 +0800

    update bat style

--- fzf-git.zsh
+++ fzf-git.zsh
@@ -13,7 +13,7 @@ gf_() {
   is_in_git_repo || return
   git -c color.status=always status --short |
   fzf-down -m --ansi --nth 2..,.. \
-    --preview '(git diff --color=always -- {-1} | sed 1,4d; cat {-1}) | head -500' |
+    --preview '(git diff --color=always -- {-1} | sed 1,4d; bat --color=always {-1}) | head -500' |
   cut -c4- | sed 's/.* -> //'
 }
 
